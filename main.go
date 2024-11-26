62d3f4876036dfe6893098b3290eb298ac50f21c
James Chen <jchen@cpacketnetworks.com>
2024-11-25 17:42:19 -0800

    random sleep

--- main.go
+++ main.go
@@ -13,9 +13,11 @@ func main() {
 		Concurrency: 100,
 	})
 
+	rand.Seed(time.Now().UnixNano())
+
 	app.Get("/", func(c fiber.Ctx) error {
 		fmt.Println("start sleeping...")
-		sleep_seconds := 2
+		sleep_seconds := rand.Intn(9) + 1
 		time.Sleep(time.Duration(sleep_seconds) * time.Second)
 		fmt.Printf("done sleeping after %d seconds!\n", sleep_seconds)
 		// Send a string response to the client
